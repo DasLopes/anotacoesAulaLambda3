4f48c28961c8db8a8b115c35ae79837307609757
daslopes <daslopes@outlook.com.br>
2024-05-20 15:53:27 -0300

    Reference method com método não estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -25,9 +25,9 @@ public class Product {
     public void setPrice(Double price) {
         this.price = price;
     }
-
-    public static void staticPriceUpdate(Product p){
-        p.setPrice(p.getPrice() * 1.1);
+    
+    public void nonStaticPriceUpdate(){
+        price *= 1.1;
     }
 
     @Override
