90352662c0e7be727ec07fa3ff730527a9dbcfb5
daslopes <daslopes@outlook.com.br>
2024-05-20 16:10:26 -0300

    "Expressão lambda inline"

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,13 +17,9 @@ public class Program {
         list.add(new Product("Table", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        double factor = 1.1;
+        double factor = 1.1;        
 
-        Consumer<Product> cons = p -> {
-            p.setPrice(p.getPrice() * factor);
-        };
-
-        list.forEach(cons);
+        list.forEach(p -> p.setPrice(p.getPrice() * factor));
 
         list.forEach(System.out::println);
     }
